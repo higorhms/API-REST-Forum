d5e217877398e8b602bd72d2126fa3f059404fa6
higorhms <higorhmscebrac@hotmail.com>
2019-07-31 21:53:28 -0300

    Aplicando algumas alterações REST no controller e adicionando o DATA TRANSFER OBJECT para transitar topicos

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,20 +3,19 @@ package br.com.alura.forum.controller;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.model.Curso;
 import br.com.alura.forum.model.Topico;
 
-@Controller
+@RestController
 public class TopicosController {
 
 	@RequestMapping("/topicos")
-	@ResponseBody
-	public List<Topico> getTopicos() {
-		Topico topico = new Topico("Duvida", "Duvida com spring", new Curso("Spring Boot", "Programacao"));
-		return Arrays.asList(topico, topico, topico);
+	public List<TopicoDto> getTopicos() {
+		Topico topico = new Topico("Duvida", "Dúvida com spring", new Curso("Spring Boot", "Programacao"));
+		return TopicoDto.converter(Arrays.asList(topico, topico, topico));
 	}
 }
