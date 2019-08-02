9c8cf6f067f7ce621fcbe60fcc962de6cd478270
higorhms <higorhmscebrac@hotmail.com>
2019-08-02 15:57:10 -0300

    adicionando o padrão REPOSITORY para acessar os dados no banco de dados H2 (Banco de dados em memoria)

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,21 +1,25 @@
 package br.com.alura.forum.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
-import br.com.alura.forum.model.Curso;
 import br.com.alura.forum.model.Topico;
+import br.com.alura.forum.model.repository.TopicoRepository;
 
 @RestController
 public class TopicosController {
 
+	@Autowired
+	private TopicoRepository topicoRepository;
+
 	@RequestMapping("/topicos")
 	public List<TopicoDto> getTopicos() {
-		Topico topico = new Topico("Duvida", "Dúvida com spring", new Curso("Spring Boot", "Programacao"));
-		return TopicoDto.converter(Arrays.asList(topico, topico, topico));
+
+		List<Topico> topicos = topicoRepository.findAll();
+		return TopicoDto.converter(topicos);
 	}
 }
