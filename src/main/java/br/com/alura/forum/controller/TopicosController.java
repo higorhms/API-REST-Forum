0514b6dacc2d1bca8fbb9083d64755fdd2552484
higorhms <higorhmscebrac@hotmail.com>
2019-08-08 17:59:51 -0300

    Finalizando o crud, e validações de respostas not found do servidor para o cliente

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -2,19 +2,26 @@ package br.com.alura.forum.controller;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
+import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import br.com.alura.forum.controller.dto.DetalheDoTopicoDto;
 import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.controller.form.AtualizacaoTopicoForm;
 import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.model.Topico;
 import br.com.alura.forum.model.repository.CursoRepository;
@@ -41,7 +48,18 @@ public class TopicosController {
 		return TopicoDto.converter(topicos);
 	}
 
+	@GetMapping("/{id}")
+	public ResponseEntity<DetalheDoTopicoDto> detalhar(@PathVariable Long id) {
+
+		Optional<Topico> optional = topicoRepository.findById(id);
+		if (optional.isPresent()) {
+			return ResponseEntity.ok(new DetalheDoTopicoDto(optional.get()));
+		}
+		return ResponseEntity.notFound().build();
+	}
+
 	@PostMapping
+	@Transactional
 	public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm topicoForm, UriComponentsBuilder uriCB) {
 
 		Topico topico = topicoForm.converter(cursoRepository);
@@ -51,4 +69,26 @@ public class TopicosController {
 		return ResponseEntity.created(uri).body(new TopicoDto(topico));
 	}
 
+	@Transactional
+	@PutMapping("/{id}")
+	public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form) {
+
+		Optional<Topico> optional = topicoRepository.findById(id);
+		if (optional.isPresent()) {
+			Topico topico = form.atualiza(id, topicoRepository);
+			return ResponseEntity.ok(new TopicoDto(topico));
+		}
+		return ResponseEntity.notFound().build();
+	}
+
+	@Transactional
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> remover(@PathVariable Long id) {
+
+		if (topicoRepository.findById(id).isPresent()) {
+			topicoRepository.deleteById(id);
+			return ResponseEntity.ok().build();
+		}
+		return ResponseEntity.notFound().build();
+	}
 }
---
+++ src/main/java/br/com/alura/forum/controller/dto/DetalheDoTopicoDto.java
@@ -0,0 +1,56 @@
+package br.com.alura.forum.controller.dto;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import br.com.alura.forum.model.StatusTopico;
+import br.com.alura.forum.model.Topico;
+
+public class DetalheDoTopicoDto {
+
+	private long id;
+	private String titulo;
+	private String mensagem;
+	private LocalDateTime dataCriacao;
+	private StatusTopico status;
+	private String nomeAutor;
+	private List<RespostaDto> respostas;
+
+	public DetalheDoTopicoDto(Topico topico) {
+		this.id = topico.getId();
+		this.titulo = topico.getTitulo();
+		this.mensagem = topico.getMensagem();
+		this.dataCriacao = topico.getDataCriacao();
+		this.status = topico.getStatus();
+		this.nomeAutor = topico.getAutor().getNome();
+		this.respostas = new ArrayList<>();
+		this.respostas.addAll(topico.getRespostas().stream().map(RespostaDto::new).collect(Collectors.toList()));
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public LocalDateTime getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public StatusTopico getStatus() {
+		return status;
+	}
+
+	public String getNomeAutor() {
+		return nomeAutor;
+	}
+
+}
