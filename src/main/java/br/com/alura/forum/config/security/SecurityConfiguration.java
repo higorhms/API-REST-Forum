c3b7229d51f94d6267e9a87eec461c39a2ed7c21
higorhms <higorhmscebrac@hotmail.com>
2019-08-20 20:33:29 -0300

    Implementando autenticação via TOKEN

--- src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
@@ -1,13 +1,16 @@
 package br.com.alura.forum.config.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity // Ativa o spring security no projeto
@@ -16,7 +19,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private AutenticationService autenticationService;
-
+ 
+	@Override // Metodo que permite ter o autentication manager no controller de autenticação
+	@Bean
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
+	
 	// Autenticação
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -32,7 +41,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		// Esse metodo informa ao spring os endereços que devem ser autorizados sem
 		// autenticação
 		http.authorizeRequests().antMatchers(HttpMethod.GET, "/topicos").permitAll()
-				.antMatchers(HttpMethod.GET, "/topicos/*").permitAll().anyRequest().authenticated().and().formLogin();
+				.antMatchers(HttpMethod.GET, "/topicos/*").permitAll().antMatchers(HttpMethod.POST, "/auth").permitAll()
+				.anyRequest().authenticated().and().csrf().disable().sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 
 	// Recursos estáticos
---
+++ src/main/java/br/com/alura/forum/config/security/TokenService.java
@@ -0,0 +1,34 @@
+package br.com.alura.forum.config.security;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import br.com.alura.forum.model.Usuario;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Service
+public class TokenService {
+
+	@Value("${forum.jwt.expiration}") // Expression language para obter o valor do atributo expiration no
+										// application.properties
+	private String expiration;
+
+	@Value("${forum.jwt.secret}") // Expression language para obter o valor do atributo expiration no
+									// application.properties
+	private String secret;
+
+	public String gerarToken(Authentication authentication) {
+		Usuario usuario = (Usuario) authentication.getPrincipal();
+		Date hoje = new Date();
+		Date dataExpiracao = new Date(hoje.getTime() + Long.parseLong(expiration));
+
+		// Utilizando a biblioteca JJWT para criação do token
+		return Jwts.builder().setIssuer("API do Higor").setSubject(usuario.getId().toString()).setIssuedAt(hoje)
+				.setExpiration(dataExpiracao).signWith(SignatureAlgorithm.HS256, secret).compact();
+	}
+
+}
